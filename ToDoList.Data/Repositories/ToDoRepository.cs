3d0e7e172b1194e82e90b2f61f13068497bb5f51
Bruno Sales <brunosbernardes01@gmail.com>
2021-05-13 19:51:20 -0300

    Remove dataContext from appService, now using repository

--- ToDoList.Data/Repositories/ToDoRepository.cs
+++ ToDoList.Data/Repositories/ToDoRepository.cs
@@ -1,4 +1,6 @@
-﻿using ToDoList.Domain.Commands.Requests;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 using ToDoList.Domain.Entities;
 using ToDoList.Domain.Interfaces;
 using ToDoList.Infra.Contexts;
@@ -12,9 +14,37 @@ namespace ToDoList.Infra.Repositories
         {
             _context = context;
         }
-        public void Create(ToDo request)
+        public void Create(ToDo toDo)
         {
-            _context.ToDo.Add(request);
+            _context.ToDo.Add(toDo);
+            _context.SaveChanges();
+        }
+
+        public void Delete(ToDo toDo)
+        {
+            _context.ToDo.Remove(toDo);
+            _context.SaveChanges();
+        }
+
+        public ICollection<ToDo> GetAll()
+        {
+            return _context.ToDo.AsNoTracking().ToList();
+        }
+
+        public ToDo GetById(int id)
+        {
+            return _context.ToDo.FirstOrDefault(x => x.Id == id);
+        }
+
+        public ToDo GetByIdAsNoTracking(int id)
+        {
+            return _context.ToDo.AsNoTracking().FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Update(ToDo toDo)
+        {
+            _context.Entry(toDo).State = EntityState.Modified;
+            _context.SaveChanges();
         }
     }
 }
--- ToDoList.Domain/Interfaces/IToDoRepository.cs
+++ ToDoList.Domain/Interfaces/IToDoRepository.cs
@@ -1,10 +1,16 @@
-﻿using ToDoList.Domain.Commands.Requests;
+﻿using System.Collections.Generic;
+using ToDoList.Domain.Commands.Requests;
 using ToDoList.Domain.Entities;
 
 namespace ToDoList.Domain.Interfaces
 {
     public interface IToDoRepository
     {
-        void Create(ToDo request);
+        void Create(ToDo toDo);
+        void Update(ToDo toDo);
+        void Delete(ToDo toDo);
+        ToDo GetById(int id);
+        ToDo GetByIdAsNoTracking(int id);
+        ICollection<ToDo> GetAll();
     }
 }
